2c72e3609e863d3453bbad822d1f8574f3191bff
darkkkCoDeR <dadayaalex@gmail.com>
2026-08-27 22:55:22 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,10 +6,14 @@ from unittest import mock
 @pytest.mark.parametrize(
     "has_internet,is_valid_url,expected",
     [
-        pytest.param(True, True, "Accessible", id="valid_url_with_connection"),
-        pytest.param(True, False, "Not accessible", id="invalid_url_with_connection"),
-        pytest.param(False, True, "Not accessible", id="valid_url_without_connection"),
-        pytest.param(False, False, "Not accessible", id="invalid_url_without_connection"),
+        pytest.param(True, True, "Accessible",
+                     id="valid url with connection"),
+        pytest.param(True, False, "Not accessible",
+                     id="invalid url with connection"),
+        pytest.param(False, True, "Not accessible",
+                     id="valid url without connection"),
+        pytest.param(False, False, "Not accessible",
+                     id="invalid_url_without_connection"),
     ]
 )
 @mock.patch("app.main.has_internet_connection")
