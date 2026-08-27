830fbbc264263d5387dcdd62a055c35a9dee2d60
darkkkCoDeR <dadayaalex@gmail.com>
2026-08-27 20:43:02 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,35 +1,27 @@
+import pytest
 from app.main import can_access_google_page
 from unittest import mock
 
 
+@pytest.mark.parametrize(
+    "has_internet,is_valid_url,expected",
+    [
+        pytest.param(True, True, "Accessible", id="Connection "),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible"),
+    ]
+)
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
 def test_can_access_google_page(
-        mocked_valid: mock.Mock,
-        mocked_has_internet: mock.Mock
+        mocked_valid_url: mock.Mock,
+        mocked_has_internet: mock.Mock,
+        has_internet: bool,
+        is_valid_url: bool,
+        expected: str,
 ) -> None:
-    mocked_has_internet.return_value = True
-    mocked_valid.return_value = True
+    mocked_has_internet.return_value = has_internet
+    mocked_valid_url.return_value = is_valid_url
+
     assert can_access_google_page("google.com") == "Accessible"
-
-
-@mock.patch("app.main.has_internet_connection")
-@mock.patch("app.main.valid_google_url")
-def test_cannot_access_with_invalid_url(
-        mocked_valid: mock.Mock,
-        mocked_has_internet: mock.Mock
-) -> None:
-    mocked_has_internet.return_value = True
-    mocked_valid.return_value = False
-    assert can_access_google_page("valid.com") == "Not accessible"
-
-
-@mock.patch("app.main.has_internet_connection")
-@mock.patch("app.main.valid_google_url")
-def test_cannot_access_with_only_valid_url(
-        mocked_valid: mock.Mock,
-        mocked_has_internet: mock.Mock
-) -> None:
-    mocked_has_internet.return_value = False
-    mocked_valid.return_value = True
-    assert can_access_google_page("valid.com") == "Not accessible"
